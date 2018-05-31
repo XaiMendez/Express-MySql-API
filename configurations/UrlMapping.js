0b570a6585ebed4ce92ce665396b311089cfeecf
Xavier <shavy_00@hotmail.com>
2018-05-30 22:11:00 -0600

    Adding Career documentation

--- configurations/UrlMapping.js
+++ configurations/UrlMapping.js
@@ -1,15 +1,5 @@
- /**
-  * @swagger
-  * definition:
-  *   User:
-  *     properties:
-  *       username:
-  *         type: string
-  *       password:
-  *         type: string
-  */
 
-   /**
+/**
   * @swagger
   * /user:
   *   get:
@@ -115,4 +105,135 @@
   *     responses:
   *       200:
   *         description: Delete user
+  */
+
+  /**
+  * @swagger
+  * /career:
+  *   get:
+  *     tags:
+  *       - Career
+  *     summary: Get all careers
+  *     description: Get all careers
+  *     operationId: findAllCareers
+  *     produces:
+  *       - application/json
+  *     parameters: []
+  *     responses:
+  *       200:
+  *         description: Get all careers
+  */
+
+ /**
+  * @swagger
+  * /career/{id}:
+  *   get:
+  *     tags:
+  *       - Career
+  *     summary: Get specific career
+  *     description: Get specific career
+  *     operationId: findCareerById
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: id
+  *         description: Career id
+  *         in: path
+  *         required: true
+  *         type: string
+  *     responses:
+  *       200:
+  *         description: Get career by id
+  */
+
+ /**
+  * @swagger
+  * /career:
+  *   post:
+  *     tags:
+  *       - Career
+  *     summary: Create career
+  *     description: Create career
+  *     operationId: createCareer
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: body
+  *         description: Career object that needs to be added to the database
+  *         in: body
+  *         required: true
+  *         schema:
+  *           $ref: '#/definitions/Career'
+  *     responses:
+  *       201:
+  *         description: Return saved career
+  *         schema:
+  *           $ref: '#/definitions/Career'
+  */
+
+ /**
+  * @swagger
+  * /career:
+  *   put:
+  *     tags:
+  *       - Career
+  *     summary: Update career
+  *     description: Update specific career detail
+  *     operationId: updateCareerById
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: body
+  *         description: Career object that needs to be updated to the database
+  *         in: body
+  *         required: true
+  *         schema:
+  *           $ref: '#/definitions/Career'
+  *     responses:
+  *       200:
+  *         description: Update career
+  */
+
+ /**
+  * @swagger
+  * /career/{id}:
+  *   delete:
+  *     tags:
+  *       - Career
+  *     summary: Delete career
+  *     description: Delete specific career detail
+  *     operationId: deleteCareer
+  *     produces:
+  *       - application/json
+  *     parameters:
+  *       - name: id
+  *         description: Career id
+  *         in: path
+  *         required: true
+  *         type: string
+  *     responses:
+  *       200:
+  *         description: Delete career
+  */
+
+   /**
+  * @swagger
+  * definition:
+  *   User:
+  *     properties:
+  *       username:
+  *         type: string
+  *       password:
+  *         type: string
+  */
+
+     /**
+  * @swagger
+  * definition:
+  *   Career:
+  *     properties:
+  *       id:
+  *         type: integer
+  *       name:
+  *         type: string
   */
